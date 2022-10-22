2b45769890b75259bf2bca38ed724c3dd26e087b
Omkar Nag <knomkarnag24@gmail.com>
2022-10-22 20:42:51 +0530

    added authguard service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthService } from './auth/auth.service';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: 'departments', component: DepartmentListComponent },
+  { path: 'departments', component: DepartmentListComponent, canActivate: [AuthService] },
   { path: 'employees', component: EmployeeListComponent },
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,7 +21,6 @@ export class HomeComponent implements OnInit {
 
   logOut() {
     localStorage.removeItem("jwt");
-    location.reload();
   }
 
   redirectToLogin() {
